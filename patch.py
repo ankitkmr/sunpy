c7b5d3c685ba43d04050bc46b4b2efb99b23356b
Ankit Kumar <technocrat.ankit@gmail.com>
2015-03-18 01:07:06 +0530

    patch to issue 798

---
+++ patch.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import pytz
+import datetime
+import numpy as np
+from tzlocal import get_localzone
+
+tz = get_localzone()
+from sunpy.time import parse_time
+
+x=np.linspace(0,19,20)
+basetime=datetime.datetime.utcnow()
+times=[]  
+
+for thing in x:
+	times.append(basetime + datetime.timedelta(0,thing))
+
+times = pd.to_datetime(times)
+
+print "\n \n \n ======== \n"
+
+test_pandas=pd.DataFrame(np.random.random(20),index=times)
+
+print test_pandas
+
+utc = pytz.timezone('UTC')
+test_pandas.index = times.tz_localize(tz).tz_convert(utc)
+
+print "\n \n \n ======== \n"
+
+print test_pandas
+
+
+# print type(pd.to_datetime(str(test_pandas.index.values[0])).strftime('%Y.%m.%d'))
+# print pd.to_datetime(str(test_pandas.index.values[0])).strftime('%Y.%m.%d')
+print parse_time(pd.to_datetime(str(test_pandas.index.values[0])).strftime('%Y/%m/%d') )
