d63284fd0909c4ecc52ba1f9ec2251f974716fc2
Ankit Kumar <ankitkmr@users.noreply.github.com>
2015-03-18 22:57:16 +0530

    Update patch.py

--- patch.py
+++ patch.py
@@ -2,11 +2,13 @@ import pandas as pd
 import pytz
 import datetime
 import numpy as np
-from tzlocal import get_localzone
+from pytz import reference
 
 # this is just a way around through the same example as given in the issue
 
-tz = get_localzone()
+today = datetime.datetime.now()
+localtime = reference.LocalTimezone()
+
 from sunpy.time import parse_time
 
 x=np.linspace(0,19,20)
@@ -25,7 +27,7 @@ test_pandas=pd.DataFrame(np.random.random(20),index=times)
 print test_pandas
 
 utc = pytz.timezone('UTC')					#Change DONE HERE
-test_pandas.index = times.tz_localize(tz).tz_convert(utc)	#Change DONE HERE
+test_pandas.index = times.tz_localize(localtime.tzname(today)).tz_convert(utc)	#Change DONE HERE  #now using pytz
 
 print "\n \n \n ======== \n"
 
@@ -36,4 +38,5 @@ print test_pandas
 # print pd.to_datetime(str(test_pandas.index.values[0])).strftime('%Y.%m.%d')
 print parse_time(pd.to_datetime(str(test_pandas.index.values[0])).strftime('%Y/%m/%d') )	#Change DONE HERE
 
+#need to add support for numpy.datetime64 instances like 2015-03-18T12:49:22.979471000+0000 before using __str__() to parse them
 #parse_time(...) now works fine
